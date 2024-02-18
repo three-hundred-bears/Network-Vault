e713b63dd7e37b55175f484a29b66c5828c86461
Mike H <125211414+three-hundred-bears@users.noreply.github.com>
2024-02-18 16:52:21 -0500

    refactored utility module, refactored half of net_load module

--- python/network_saver/utility.py
+++ python/network_saver/utility.py
@@ -2,6 +2,34 @@
 import os
 from pathlib import Path
 
+
+def _remap_node_categories(category_name):
+
+    map = {
+        'Shop': 'SHOP',
+        'CopNet': 'IMG',
+        'Cop2': 'COP2',
+        'ChopNet': 'CHOPNET',
+        'Chop': 'CHOP',
+        'Object': 'OBJ',
+        'Driver': 'ROP', 
+        'Sop': 'SOP',
+        'Vop': 'VOP',
+        'Lop': 'LOP',
+        'TopNet': 'TOPNET',
+        'Top': 'TOP',
+        'Dop': 'DOP'
+    }
+    conformed_cat = map.get(category_name)
+    if not conformed_cat:
+        raise RuntimeError(
+            'Unsupported context?!\n'
+            'Please send me a message telling me to ' 
+            'account for {}'.format(category_name)
+            )
+    return conformed_cat
+
+
 def get_data_dir():  
     cur_dir = Path(__file__)
     return os.path.join(cur_dir.parents[2], 'data')
@@ -15,72 +43,13 @@ def get_vault_dir():
     with open(vault_file, 'r') as f:
         return f.readline().strip()
 
-def conform_network_context(node_type):
-    if node_type.isManager():
-        context = node_type.childTypeCategory().name()
-    else:
-        context = node_type.category().name()
-    map = {
-        'Shop': 'SHOP',
-        'CopNet': 'IMG',
-        'Cop2': 'COP2',
-        'ChopNet': 'CHOPNET',
-        'Chop': 'CHOP',
-        'Object': 'OBJ',
-        'Driver': 'ROP', 
-        'Sop': 'SOP',
-        'Vop': 'VOP',
-        'Lop': 'LOP',
-        'TopNet': 'TOPNET',
-        'Top': 'TOP',
-        'Dop': 'DOP'
-    }
-    conformed_context = map.get(context)
-    if not conformed_context:
-        raise RuntimeError(
-            'Unsupported context?!\n'
-            'Please send me a message telling me to account for {}'.format(context)
-            )
-    return conformed_context
-
 
 def get_node_context(node):
-    return conform_network_context(node.type())
+    return _remap_node_categories(node.type().category().name())
 
 
-def get_network_context(network):
-    return get_node_context(network[0])
-
-
-def get_child_context(node):
-    # TODO: you already know
+def get_network_context(node):
     category = node.type().childTypeCategory()
     if not category:
         return get_node_context(node)
-    context = category.name()
-    map = {
-        'Shop': 'SHOP',
-        'CopNet': 'IMG',
-        'Cop2': 'COP2',
-        'ChopNet': 'CHOPNET',
-        'Chop': 'CHOP',
-        'Object': 'OBJ',
-        'Driver': 'ROP', 
-        'Sop': 'SOP',
-        'Vop': 'VOP',
-        'Lop': 'LOP',
-        'TopNet': 'TOPNET',
-        'Top': 'TOP',
-        'Dop': 'DOP'
-    }
-    conformed_context = map.get(context)
-    if not conformed_context:
-        raise RuntimeError(
-            'Unsupported context?!\n'
-            'Please send me a message telling me to account for {}'.format(context)
-            )
-    return conformed_context
-
-    
-
-
+    return _remap_node_categories(category.name())
